b287983ddaee366305f6068fd22e5b665ba44c8f
Antônio <pudimnee@gmail.com>
2024-11-08 20:51:34 -0300

    Update Exercicio_4.js

--- Exercicio_4.js
+++ Exercicio_4.js
@@ -1,28 +1,18 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
 console.clear();
 
-//variaveis
-var num;
+// Entrada
+console.log(`${"=".repeat(12)} Algoritmo que informa se um número é divisível por 10, 5 ou 2 ${"=".repeat(12)}\n`);
+const num = Number(prompt("Digite um número: "));
 
-//entrada
-console.log(`${"=".repeat(12)} Algoritimo que informa se um numero é divisivel por 10, 5 ou 2 ${"=".repeat(12)}\n`);
+// Saída
+const divisores = [];
+if (num % 10 === 0) divisores.push("10");
+if (num % 5 === 0) divisores.push("5");
+if (num % 2 === 0) divisores.push("2");
 
-num = prompt("Digite um Numero: ");
-
-//saida
-if (num % 10 == 0 && num % 5 == 0 && num % 2 == 0) {
-    console.log("O numero pode ser dividido por 10, 5 e 2");
-}
-else if (num % 10 == 0) {
-    console.log("O numero pode ser dividido por 10");
-}
-else if (num % 5 == 0) {
-    console.log("O numero pode ser dividido por 5");
-}
-else if (num % 2 == 0) {
-    console.log("O numero pode ser dividido por 2");
-}
-else {
-    console.log("O numero não pode ser dividido nem por 10, nem 5, nem 2");
+if (divisores.length > 0) {
+    console.log(`O número pode ser dividido por: ${divisores.join(', ')}`);
+} else {
+    console.log("O número não pode ser dividido nem por 10, nem 5, nem 2");
 }
