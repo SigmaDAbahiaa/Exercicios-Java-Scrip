5dd876612e0a41b2d4580cd042afab06babe28db
Antônio <pudimnee@gmail.com>
2024-11-08 20:58:13 -0300

    Update Exercicio_10.js

--- Exercicio_10.js
+++ Exercicio_10.js
@@ -3,16 +3,11 @@ console.clear();
 
 // Entrada
 console.log(`${"=".repeat(12)} Algoritmo que exibe sua idade em dias ${"=".repeat(12)}\n`);
-console.log("Digite sua data de nascimento com dia, mês e ano separados por '-' (ex: 00-00-0000)\n");
-const dataNascimento = prompt("Data de Nascimento: ");
+const dataNascimento = prompt("Data de Nascimento (dd-mm-aaaa): ");
 
 // Processamento
 const [dia, mes, ano] = dataNascimento.split("-").map(Number);
 const dataAtual = new Date();
-const idadeEmAnos = dataAtual.getFullYear() - ano;
-const mesesDesdeNascimento = dataAtual.getMonth() - (mes - 1); // Ajuste para meses começando em 0
-const diasDesdeNascimento = dataAtual.getDate() - dia;
+const idadeEmDias = Math.floor((dataAtual - new Date(ano, mes - 1, dia)) / (1000 * 60 * 60 * 24));
 
-const idadeEmDias = (idadeEmAnos * 365) + (mesesDesdeNascimento * 30) + diasDesdeNascimento;
-
-console.log(`\nSua idade expressa em dias é aproximadamente: ${idadeEmDias.toFixed(0)} dias`);
+console.log(`\nSua idade expressa em dias é aproximadamente: ${idadeEmDias} dias`);
