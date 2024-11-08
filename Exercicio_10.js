87d9bb8b9b87d00cc526a87563354ec855ebab0a
Antônio <pudimnee@gmail.com>
2024-11-08 20:54:41 -0300

    Update Exercicio_10.js

--- Exercicio_10.js
+++ Exercicio_10.js
@@ -1,35 +1,18 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
 console.clear();
 
-//variaveis
-const time = new Date()
-const ano = time.getFullYear();
-var idadeEmDias;
-var AnoNascimento;
-var mesNascimento = 0;
-var idade = [];
-var Data;
+// Entrada
+console.log(`${"=".repeat(12)} Algoritmo que exibe sua idade em dias ${"=".repeat(12)}\n`);
+console.log("Digite sua data de nascimento com dia, mês e ano separados por '-' (ex: 00-00-0000)\n");
+const dataNascimento = prompt("Data de Nascimento: ");
 
-//entrada
-console.log(`${"=".repeat(12)} Algoritimo que exibe sua idade em dias ${"=".repeat(12)}\n`);
+// Processamento
+const [dia, mes, ano] = dataNascimento.split("-").map(Number);
+const dataAtual = new Date();
+const idadeEmAnos = dataAtual.getFullYear() - ano;
+const mesesDesdeNascimento = dataAtual.getMonth() - (mes - 1); // Ajuste para meses começando em 0
+const diasDesdeNascimento = dataAtual.getDate() - dia;
 
-console.log("Digite sua data de nascimento com dia, mês e ano separados por '-' deste jeito: 00-00-0000\n");
-
-Data = prompt("Data de Nascimento: ");
-Data = Number((Data.split("-").reverse().join("")))
-
-//saida
-idade[0] = Number(String(Data).substring(0, 4));
-idade[1] = Number(String(Data).substring(4, 6));
-idade[2] = Number(String(Data).substring(6));
-
-AnoNascimento = ano - idade[0];
-
-for (i = idade[1]; i < 12; i++) {
-    mesNascimento++;
-}
-
-idadeEmDias = (AnoNascimento * 365.5) + (mesNascimento * 12) + idade[2];
+const idadeEmDias = (idadeEmAnos * 365) + (mesesDesdeNascimento * 30) + diasDesdeNascimento;
 
 console.log(`\nSua idade expressa em dias é aproximadamente: ${idadeEmDias.toFixed(0)} dias`);
