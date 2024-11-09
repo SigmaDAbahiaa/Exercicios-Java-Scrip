ffd1a198e486ff17eecc175447e2c3cfd58e0859
Antônio <pudimnee@gmail.com>
2024-11-08 21:10:29 -0300

    Update Exercicio_7.js

--- Exercicio_7.js
+++ Exercicio_7.js
@@ -1,30 +1,21 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
 console.clear();
 
-//variaveis
-var CustoFabrica ;
-var ComicaoRevendedor;
-var inpostos;
-var PrecoFinal;
+// Entrada
+console.log(`${"=".repeat(12)} Algoritmo que determina a comissão de um vendedor ${"=".repeat(12)}\n`);
+console.log(`Para descobrir o valor final do revendedor, considere que 25% é a comissão e 45% são impostos.\n`);
 
-//entrada
-console.log(`${"=".repeat(12)} Algoritimo que determina a comissão de um vendedor ${"=".repeat(12)}\n`);
+const custoFabrica = Number(prompt("Digite o custo de fábrica: "));
 
-console.log(`Para descobrir o valor final do revendedor levando em consideração 25% que é a
-porcentagem do revendedor e que 45% são os inpostos aplicados ao custo de fabrica.\n
-`);
-    
-CustoFabrica = Number(prompt("Digite o custo de fabrica: "));
+// Cálculo
+const comissaoRevendedor = custoFabrica * 0.25;
+const impostos = custoFabrica * 0.45;
+const precoFinal = custoFabrica + comissaoRevendedor + impostos;
 
-//saida
-ComicaoRevendedor = CustoFabrica * 0.25;
-inpostos = CustoFabrica * 0.45;
-PrecoFinal = CustoFabrica + ComicaoRevendedor + inpostos;
-
-console.log(`\nO custo de fabrica era de: ${CustoFabrica.toFixed(2)}
-O percentual de 25% do revendedor ficou por: ${ComicaoRevendedor.toFixed(2)}
-E os inpostos ficaram com o valor de: ${inpostos.toFixed(2)}
-
-Somando tudo, o preço final de seu carro ficou por: ${Number(PrecoFinal).toFixed(2)}
+// Saída
+console.log(`
+Custo de fábrica: R$ ${custoFabrica.toFixed(2)}
+Comissão do revendedor (25%): R$ ${comissaoRevendedor.toFixed(2)}
+Impostos (45%): R$ ${impostos.toFixed(2)}
+Preço final do carro: R$ ${precoFinal.toFixed(2)}
 `);
