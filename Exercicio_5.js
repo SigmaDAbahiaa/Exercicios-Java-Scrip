7b0c25b2d7ae130b041240ca642274cb1980a22c
Antônio <pudimnee@gmail.com>
2024-11-08 21:09:34 -0300

    Update Exercicio_5.js

--- Exercicio_5.js
+++ Exercicio_5.js
@@ -6,22 +6,12 @@ console.log(`${"=".repeat(12)} Algoritmo que calcula seu IMC ${"=".repeat(12)}\n
 const peso = Number(prompt("Digite seu peso (Kg): "));
 const altura = Number(prompt("Digite sua altura (M): "));
 
-// Cálculo do IMC
+// Cálculo e Saída do IMC
 const imc = peso / (altura ** 2);
+const categoria = imc < 20 ? "abaixo do peso" :
+                  imc < 25 ? "saudável" :
+                  imc < 30 ? "sobrepeso" :
+                  imc < 40 ? "obeso" : "obeso mórbido";
+
 console.log(`\nSeu IMC é aproximadamente: ${imc.toFixed(2)}`);
-
-// Saída de resultado
-let categoria;
-if (imc < 20) {
-    categoria = "abaixo do peso";
-} else if (imc < 25) {
-    categoria = "saudável";
-} else if (imc < 30) {
-    categoria = "sobrepeso";
-} else if (imc < 40) {
-    categoria = "obeso";
-} else {
-    categoria = "obeso mórbido";
-}
-
 console.log(`Você está ${categoria}\n`);
