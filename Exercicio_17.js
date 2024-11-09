cddef7c4b1ce4c50c2723c63d258005bf1a3959c
Antônio <pudimnee@gmail.com>
2024-11-08 21:08:31 -0300

    Update Exercicio_17.js

--- Exercicio_17.js
+++ Exercicio_17.js
@@ -1,33 +1,17 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
 console.clear();
 
-//variaveis
-const vetor = Array(15);
-var ValorBuscado = 0;
+// Entrada
+console.log(`${"=".repeat(12)} Algoritmo que lê um vetor e mostra o número de ocorrências ${"=".repeat(12)}\n`);
+console.log("Digite 15 números inteiros:");
 
-//entrada
-console.log(`${"=".repeat(12)} Algoritimo que le um vetor de é mostra o numero de ocorrencias ${"=".repeat(12)}\n`);
-console.log("Para realizar o cauculo é preciso que você digite 15 numeros inteiros");
+const vetor = Array.from({ length: 15 }, (_, i) => {
+    return Number(prompt(`Digite o número para a posição ${i + 1}: `));
+});
 
-for (i = 0; i < vetor.length; i++){
-    console.log(`\nDigite o numero para a posição ${i + 1}, do vetor`);
-    vetor[i] = prompt("C/> ");
+// Busca
+const valorBuscado = Number(prompt("\nDigite o número que deseja buscar no vetor: "));
+const contador = vetor.filter(num => num === valorBuscado).length;
 
-    console.clear();
-    console.log(`numero adicionado na posição ${i + 1}.`);
-}
-
-console.log("\nDigite o numero que deseja buscar no vetor");
-ValorBuscado = prompt("C/> ");
-
-//saida
-let contador = 0;
-
-for (i = 0; i < vetor.length; i++){
-    if (vetor[i] == ValorBuscado){
-        contador++;
-    }
-}
-
-console.log(`\nO número ${ValorBuscado} aparece ${contador} vez(es) no vetor.`);
+// Saída
+console.log(`\nO número ${valorBuscado} aparece ${contador} vez(es) no vetor.`);
