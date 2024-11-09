346270cb8945bb8511b88764f3367af1cdafa61c
Antônio <pudimnee@gmail.com>
2024-11-08 21:06:08 -0300

    Update Exercicio_14.js

--- Exercicio_14.js
+++ Exercicio_14.js
@@ -1,33 +1,21 @@
-//inicio do algoritimo
 const prompt = require('prompt-sync')();
 console.clear();
 
-//variaveis
-var renda_mensal;
-var valor_emprestimo;
-var numero_prestacoes;
-var valor_prestacao;
+// Entrada
+console.log(`${"=".repeat(12)} Algoritmo que informa se um empréstimo pode ser concedido ${"=".repeat(12)}\n`);
+const rendaMensal = Number(prompt("Renda Mensal: "));
+const valorEmprestimo = Number(prompt("Valor do Empréstimo: "));
+const numeroPrestacoes = Number(prompt("Número de Prestações: "));
 
-//entrada
-console.log(`${"=".repeat(12)} Algoritimo que informa se um emprestimo pode ser concedido ${"=".repeat(12)}\n`);
-
-renda_mensal = prompt("Renda Mensal: ");
-console.log("");
-valor_emprestimo = prompt("Valor do emprestimo: ");
-console.log("");
-numero_prestacoes = prompt("N° de Prestações: ");
-
-//saida
-if (valor_emprestimo > 10 * renda_mensal){
-    console.log("\nO valor do emprestimo excede o limite permitido.");
-}
-else{
-    valor_prestacao = valor_emprestimo / numero_prestacoes;
-
-    if (valor_prestacao > 0.3 * renda_mensal){
+// Cálculo e Saída
+if (valorEmprestimo > 10 * rendaMensal) {
+    console.log("\nO valor do empréstimo excede o limite permitido.");
+} else {
+    const valorPrestacao = valorEmprestimo / numeroPrestacoes;
+    
+    if (valorPrestacao > 0.3 * rendaMensal) {
         console.log("\nO valor da prestação excede 30% da renda mensal.");
-    }
-    else{
-        console.log("\nO emprestimo pode ser concedido.");
+    } else {
+        console.log("\nO empréstimo pode ser concedido.");
     }
 }
